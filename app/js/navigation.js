b07246f4cae046b1b589905c32fdb04b08c79603
Emil Skoldator <emil.linden@gtg-educ.com>
2022-04-27 09:35:19 +0200

    Added docstring comments using Trelent

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,4 +1,11 @@
 
+/**
+ * The labbar function is used to display the labbar interface.
+ * 
+ * @return The labbar page
+ * @docauthor Trelent
+ * @docmodifier Emil Lindén
+ */
 function labbar() {
     if (there_is_element == true && z != null) { remove_element(); }
     if (there_is_skop == true) { remove_skop(); }
@@ -11,6 +18,13 @@ function labbar() {
     document.getElementById("bodySection").style.display = "flex";
 }
 
+/**
+ * The home function is used to display the home interface.
+ * 
+ * @return The home page
+ * @docauthor Trelent
+ * @docmodifier Emil Lindén
+ */
 function home() {
     if (there_is_element == true && z != null) { remove_element(); }
     if (there_is_skop == true) { remove_skop(); }
@@ -23,6 +37,12 @@ function home() {
     document.getElementById("labbarInterface").style.display = "none";
 }
 
+/**
+ * The admin function is used to display the admin interface.
+ * 
+ * @return The admin section of the labbar
+ * @docauthor Trelent
+ */
 function admin() {
     document.getElementById("bodySection").style.display = "none";
     document.getElementById("labbarInterface").style.display = "none";
