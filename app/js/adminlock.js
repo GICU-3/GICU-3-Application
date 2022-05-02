a4c831d122683a717f83f5e0142fcb287ba4d179
031simon <simon.k.hellsing@gmail.com>
2022-05-02 14:52:36 +0200

    added code that checks for output changes on the raspberry

--- app/js/adminlock.js
+++ app/js/adminlock.js
@@ -1,6 +1,6 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var admin_key = new Gpio(4, 'in'); //use GPIO pin 4, and specify that it is output
-var admin_panel = setInterval(blinkLED, 250);
+var lock_checkstatus = setInterval(admin_panel, 250); //use clearInterval(lock_checkstatus) if you want to stop the checking for some reason
 
 function admin_panel() {
     if (admin_key.readSync() === 1) { //check the pin state, if the state is 0 (or off)
