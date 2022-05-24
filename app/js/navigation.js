d650db2a4504acb2ee495bbe13570ac4dcde45e8
031simon <simon.k.hellsing@gmail.com>
2022-05-24 13:15:09 +0200

    commented away code about Labbar

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -5,7 +5,7 @@
  * @docauthor Trelent
  * @docmodifier Emil Lindén
  */
-function labbar() {
+/* function labbar() {                Labbar commented away, feature not done
     if (there_is_element == true && z != null) { remove_element(); }
     if (there_is_skop == true) { remove_skop(); }
     z = null;
@@ -19,7 +19,7 @@ function labbar() {
     document.getElementById("bookimg").style.filter = "invert(55%) sepia(0%) saturate(0%) hue-rotate(174deg) brightness(91%) contrast(90%)";
     document.getElementById("homeimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";
     document.getElementById("engineerimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";
-}
+}*/
 
 /**
  * The home function is used to display the home interface.
