7cc74cd47bb0b934ec99ca521b7d22e827c3cb45
031simon <simon.k.hellsing@gmail.com>
2022-05-30 14:33:05 +0200

    commented away bookimg in navigation.js

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -40,7 +40,7 @@ function home() {
     document.getElementById("searchbar").style.display = "block";
     document.getElementById("labbarInterface").style.display = "none";
     document.getElementById("homeimg").style.filter = "invert(55%) sepia(0%) saturate(0%) hue-rotate(174deg) brightness(91%) contrast(90%)";
-    document.getElementById("bookimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";
+    //document.getElementById("bookimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";        Labbar commented away, feature not done
     document.getElementById("engineerimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";
 }
 
@@ -56,7 +56,7 @@ function admin() {
     document.getElementById("admin_s").style.display = "block";
     document.getElementById("engineerimg").style.filter = "invert(55%) sepia(0%) saturate(0%) hue-rotate(174deg) brightness(91%) contrast(90%)";
     document.getElementById("homeimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";
-    document.getElementById("bookimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";
+    //document.getElementById("bookimg").style.filter = "invert(100%) sepia(100%) saturate(100%) hue-rotate(157deg) brightness(100%) contrast(100%)";     Labbar commented away, feature not done
     document.getElementById("admin_s").style.display = "grid";
     settings();
 }
