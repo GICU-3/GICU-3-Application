9cd4ae1fb3bb87771b96dfc4b45e8fb58cd1431b
MeName420 <plamen.nenov@aol.se>
2022-06-07 19:17:28 +0200

    change in amount of cabinets, bug fixed

--- app/js/adminlock.js
+++ app/js/adminlock.js
@@ -4,7 +4,7 @@ var dgram = require('dgram');
 var server = dgram.createSocket("udp4");
 var client = dgram.createSocket("udp4");
 server.bind(8090);
-setInterval(admin_panel, 1000); //use clearInterval(lock_checkstatus) if you want to stop the checking for some reason
+//setInterval(admin_panel, 1000); //use clearInterval(lock_checkstatus) if you want to stop the checking for some reason
 
 /**
  * The admin_panel function is used to display the admin panel when the pin is turned on.
@@ -19,14 +19,13 @@ function admin_panel() { //TODO rename to adminPanel(). MUST BE CHANGED IN A SHI
         document.getElementById("admin").style.display = "block";
     } else {
         document.getElementById("admin").style.display = "none";
-            home();
-            document.querySelector(".navbar").style.display = "block";
-            try {navigation_database_con.parentNode.removeChild(navigation_database_con)} catch{}
-        document.onkeydown = function(e) {
-        };
+        home();
+        document.querySelector(".navbar").style.display = "block";
+        try { navigation_database_con.parentNode.removeChild(navigation_database_con) } catch {}
+        document.onkeydown = function(e) {};
     }
 }
 
-server.on("message", function (message) {
+server.on("message", function(message) {
     admin_lock_value = message.toString();
 })
